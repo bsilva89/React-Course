99b7c14c8889a162ba82e0e63d87ee9dec59d6a0
Bruno Santos <bsilva89@hotmail.com>
2020-08-27 17:09:41 -0300

    adding first app structure

--- react-course-folder/2-first-app/src/components/Layout/Layout.js
+++ react-course-folder/2-first-app/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from '../../hoc/Auxiliary.js'
+import classes from './Layout.css'
 
 const layout = ( props ) => (
     <Aux>
         <React.Fragment>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <main>
+            <main className={classes}>
                 {props.children}
             </main>
         </React.Fragment>
