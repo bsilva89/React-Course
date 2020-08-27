b62047007b56982da3908fd0f2dee8fc77960acf
Bruno Santos <bsilva89@hotmail.com>
2020-08-27 20:38:16 -0300

    init first app

--- react-course-folder/2-first-app/src/components/Layout/Layout.js
+++ react-course-folder/2-first-app/src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Aux from '../../hoc/Auxiliary.js'
-import classes from './Layout.css'
+import Aux from '../../hoc/Auxiliary.js';
+import classes from './Layout.module.css';
 
 const layout = ( props ) => (
     <Aux>
         <React.Fragment>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <main className={classes}>
+            <main className={classes.Content}>
                 {props.children}
             </main>
         </React.Fragment>
